339ce4d688668a1399a9c5815474a2c041813aaf
unknown <khaireddine99>
2025-05-30 15:11:07 +0100

    migrations applied

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    path('', views.create_checkout_session, name='index')
+    path('', views.index, name='index')
 ]
--- payment/views.py
+++ payment/views.py
@@ -2,30 +2,9 @@ import stripe
 from django.conf import settings  # ✅ Import from Django settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
 
 stripe.api_key = settings.STRIPE_SECRET_KEY  # ✅ Use what's in settings
 
-@csrf_exempt
-def create_checkout_session(request):
-    try:
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[
-                {
-                    'price_data': {
-                        'currency': 'usd',
-                        'unit_amount': 2000,  # amount in cents ($20)
-                        'product_data': {
-                            'name': 'Cool T-shirt',
-                        },
-                    },
-                    'quantity': 1,
-                },
-            ],
-            mode='payment',
-            success_url='http://localhost:8000/success/',
-            cancel_url='http://localhost:8000/cancel/',
-        )
-        return JsonResponse({'id': session.id})
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=400)
+def index(request):
+    return render(request, 'payment/index.html')
